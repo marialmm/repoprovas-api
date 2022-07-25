7211d38ea53bb0ee61d1d5d9c6876d121290f3ff
Maria Luiza <maria.lmm.975@gmail.com>
2022-07-25 14:40:24 -0300

    feat: add Get test tests

--- src/tests/factories/testFactory.ts
+++ src/tests/factories/testFactory.ts
@@ -1,5 +1,7 @@
 import { faker } from "@faker-js/faker";
 
+import prisma from "../../config/database.js";
+
 export function createTestData() {
     const name = faker.lorem.sentence(4);
     const pdfUrl = faker.internet.url();
@@ -8,4 +10,59 @@ export function createTestData() {
     const disciplineId = 1;
 
     return { name, pdfUrl, disciplineId, teacherId, categoryId };
+}
+
+interface testData {
+    name: string;
+    pdfUrl: string;
+    categoryId: number;
+    teacherId: number;
+    disciplineId: number;
+    teacherDisciplineId: number;
+}
+
+export async function createNewTest(testData: testData) {
+    testData.teacherDisciplineId = 1;
+    delete testData.teacherId;
+    delete testData.disciplineId;
+
+    await prisma.test.create({
+        data: testData,
+    });
+}
+
+export async function getTestsByDiscipline() {
+    const tests = await prisma.term.findMany({
+        include: {
+            disciplines: {
+                include: {
+                    teacherDisciplines: {
+                        select: {
+                            id: true,
+                            discipline: {
+                                select: {
+                                    name: true,
+                                },
+                            },
+                            teacher: {
+                                select: {
+                                    name: true,
+                                },
+                            },
+                            tests: {
+                                select: {
+                                    id: true,
+                                    name: true,
+                                    pdfUrl: true,
+                                    category: true,
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    });
+
+    return tests;
 }
--- src/tests/test.test.js
+++ src/tests/test.test.js
@@ -35,6 +35,21 @@ describe("Post test tests", () => {
         });
     });
 
+    it("Given an invalid input, should return 422", async () => {
+        const testData = {};
+        const token = await userFactory.createToken();
+        const header = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const response = await supertest(app)
+            .post("/test")
+            .set(header)
+            .send(testData);
+
+        expect(response.statusCode).toBe(422);
+    });
+
     it("Given an invalid token, should return 401", async () => {
         const testData = testFactory.createTestData();
         const header = {
@@ -81,6 +96,57 @@ describe("Post test tests", () => {
             .send(testData);
 
         expect(response.statusCode).toEqual(400);
+    });
+});
+
+describe("Get test tests", () => {
+    it("Given a correct query string, should return tests by disciplines and status 200", async () => {
+        const token = await userFactory.createToken();
+        const header = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const testData = testFactory.createTestData();
+        await testFactory.createNewTest(testData);
+
+        const response = await supertest(app)
+            .get("/tests?groupBy=disciplines")
+            .set(header);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.tests).not.toBeUndefined();
+
+        const tests = await testFactory.getTestsByDiscipline();
+
+        expect(response.body.tests).toEqual(tests);
+    });
+
+    it("Given an invalid token, should return 401", async () => {
+        const header = {
+            Authorization: `Bearer invalidToken`,
+        };
+
+        const response = await supertest(app)
+            .get("/tests?groupBy=disciplines")
+            .set(header);
+
+        expect(response.statusCode).toBe(401);
+    });
+
+    it("Given an incorrect query string, should return 400", async () => {
+        const token = await userFactory.createToken();
+        const header = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const testData = testFactory.createTestData();
+        await testFactory.createNewTest(testData);
+
+        const response = await supertest(app)
+            .get("/tests")
+            .set(header);
+
+        expect(response.statusCode).toBe(400);
     });
 });
 
